bd4bda37cfc1aad7ab2509024aab9c7a7abe238f
psh140 <psh140140@gmail.com>
2020-06-24 17:34:08 +0900

    Bottom Navigation View Add

--- app/src/main/java/com/hugh/instagram_clone_ver_02/MainActivity.kt
+++ app/src/main/java/com/hugh/instagram_clone_ver_02/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.hugh.instagram_clone_ver_02
 
 import android.os.Bundle
+import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -10,17 +12,57 @@ import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var textView: TextView
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.nav_home -> {
+//                moveToFragment(HomeFragment())
+                textView.setText("Home")
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_search -> {
+//                moveToFragment(SearchFragment())
+                textView.setText("Search")
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_add_post -> {
+                textView.setText("Add post")
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_notifications -> {
+//                moveToFragment(NotificationsFragment())
+                textView.setText("Notification")
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_profile -> {
+//                moveToFragment(ProfileFragment())
+                textView.setText("Profile")
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+
+        false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        textView = findViewById(R.id.message)
+
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+//        moveToFragment(HomeFragment())
+
     }
+
+
+//    private fun moveToFragment(fragment: Fragment)
+//    {
+//        val fragmentTrans = supportFragmentManager.beginTransaction()
+//        fragmentTrans.replace(R.id.fragment_container, fragment)
+//        fragmentTrans.commit()
+//    }
 }
