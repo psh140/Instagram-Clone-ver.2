735881dee430fd281984491a779b399a71df6289
psh140 <psh140140@gmail.com>
2020-07-01 19:53:02 +0900

    Login And Sign up UI Add

---
+++ app/src/main/java/com/hugh/instagram_clone_ver_02/SignInActivity.kt
@@ -0,0 +1,17 @@
+package com.hugh.instagram_clone_ver_02
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sign_in.*
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        signup_link_btn.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
+    }
+}
